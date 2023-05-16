d90eeaa1ad0c0be8262ecd2186b31eacfa4378cc
Meshate <li19638176@gmail.com>
2023-05-16 19:01:32 +0800

    save

--- internal/client/base.go
+++ internal/client/base.go
@@ -1,6 +1,10 @@
 package client
 
-import "github.com/OPPOGROUP/hoyolib/internal/utils/request"
+import (
+	"github.com/OPPOGROUP/hoyolib/internal/user"
+	"github.com/OPPOGROUP/hoyolib/internal/utils/request"
+	"time"
+)
 
 type IClient interface {
 	Sign() error
@@ -11,10 +15,36 @@ type Client struct {
 	ActId              string
 	SignInfoUrl        string
 	SignUrl            string
+	userInfo           *user.Info
 	accountInfoRequest *request.Request
+	signInfoRequest    *request.Request
 	signRequest        *request.Request
+	cancel             chan struct{}
+}
+
+func (c *Client) Loop() {
+	cancel := make(chan struct{}, 1)
+	c.updateAccountInfo()
+
+	tick := time.NewTicker(1 * time.Hour)
+	go func(cancel chan struct{}) {
+		defer tick.Stop()
+		for range tick.C {
+			select {
+			case <-cancel:
+				return
+			default:
+				c.updateAccountInfo()
+			}
+		}
+	}(cancel)
+	c.cancel = cancel
 }
 
 func (c *Client) Sign() error {
 	return nil
+}
+
+func (c *Client) updateAccountInfo() {
+
 }
