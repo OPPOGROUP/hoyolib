f6de437c809a238a87331fc9a02d529422a7c727
Meshate <li19638176@gmail.com>
2023-05-19 19:14:23 +0800

    [update] save work progress. Finish log

--- internal/log/logger.go
+++ internal/log/logger.go
@@ -7,6 +7,7 @@ import (
 	"github.com/spf13/viper"
 	"os"
 	"path"
+	"strings"
 	"time"
 )
 
@@ -33,11 +34,28 @@ func Init() error {
 	fileName := path.Join(dir, fmt.Sprintf("%s.log", time.Now().Format("2006-01-02")))
 	log.Debug().Str("fileName", fileName)
 	logFile, _ := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	var writer zerolog.LevelWriter
 	if viper.GetString("env") == "prod" {
-		writer := zerolog.MultiLevelWriter(logFile)
-		logger = zerolog.New(writer).Level(level).With().Timestamp().Logger()
-		logger.s
+		writer = zerolog.MultiLevelWriter(logFile)
 	} else {
-
+		consoleWriter := zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: "2006-01-02 15:04:05",
+		}
+		consoleWriter.FormatLevel = func(i interface{}) string {
+			return strings.ToUpper(fmt.Sprintf("|%-6s|", i))
+		}
+		consoleWriter.FormatMessage = func(i interface{}) string {
+			return fmt.Sprintf("%s", i)
+		}
+		consoleWriter.FormatFieldName = func(i interface{}) string {
+			return fmt.Sprintf("%s: ", i)
+		}
+		consoleWriter.FormatFieldValue = func(i interface{}) string {
+			return fmt.Sprintf("%s", i)
+		}
+		writer = zerolog.MultiLevelWriter(logFile, consoleWriter)
 	}
+	logger = zerolog.New(writer).Level(level)
+	return nil
 }
