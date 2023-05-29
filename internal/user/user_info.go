11db49153695a2d54526ea4f3cbcd2a821af46d8
Meshate <li19638176@gmail.com>
2023-05-30 01:59:41 +0800

    [update] save work progress

--- internal/user/user_info.go
+++ internal/user/user_info.go
@@ -6,8 +6,44 @@ import (
 )
 
 type Info struct {
-	AccountId   string                                                                           `json:"account_id"`
-	CookieToken string                                                                           `json:"cookie_token"`
-	ClientNotes map[hoyolib_pb.RegisterRequest_AccountType]map[hoyolib_pb.GameType]struct{}      `json:"client_note"`
-	Clients     map[hoyolib_pb.RegisterRequest_AccountType]map[hoyolib_pb.GameType]client.Client `json:"-"`
+	Active bool                                              `json:"-"`
+	Msg    string                                            `json:"-"`
+	Infos  map[hoyolib_pb.RegisterRequest_AccountType]*_info `json:"infos"`
+}
+
+type _info struct {
+	AccountId   string                                `json:"account_id"`
+	CookieToken string                                `json:"cookie_token"`
+	ClientNotes []hoyolib_pb.GameType                 `json:"client_notes"`
+	Clients     map[hoyolib_pb.GameType]client.Client `json:"-"`
+}
+
+func (i *Info) CreateClients(server hoyolib_pb.RegisterRequest_AccountType) error {
+	info := i.Infos[server]
+	info.Clients = make(map[hoyolib_pb.GameType]client.Client)
+	for _, g := range info.ClientNotes {
+		var (
+			c   client.Client
+			err error
+		)
+		switch g {
+		case hoyolib_pb.GameType_Genshin:
+			c, err = client.NewGenshinClient(server, info.AccountId, info.CookieToken)
+			if err != nil {
+				i.Active = false
+				i.Msg = err.Error()
+				return err
+			}
+		case hoyolib_pb.GameType_StarRail:
+			c, err = client.NewStarRailClient(server, info.AccountId, info.CookieToken)
+			if err != nil {
+				i.Active = false
+				i.Msg = err.Error()
+				return err
+			}
+		}
+		info.Clients[g] = c
+	}
+	i.Active = true
+	return nil
 }
