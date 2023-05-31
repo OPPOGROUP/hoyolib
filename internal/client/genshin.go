069b195f478f30f00ab51a2423f66a6cbbf5abee
Meshate <li19638176@gmail.com>
2023-05-31 18:39:18 +0800

    [update] save

--- internal/client/genshin.go
+++ internal/client/genshin.go
@@ -3,7 +3,6 @@ package client
 import (
 	"fmt"
 	"github.com/OPPOGROUP/hoyolib/internal/cte"
-	"github.com/OPPOGROUP/hoyolib/internal/errors"
 	"github.com/OPPOGROUP/hoyolib/internal/utils"
 	"github.com/OPPOGROUP/hoyolib/internal/utils/request"
 	"github.com/OPPOGROUP/protocol/hoyolib_pb"
@@ -42,9 +41,18 @@ func NewGenshinClient(server hoyolib_pb.RegisterRequest_AccountType, accountId, 
 			"account_id":   accountId,
 			"cookie_token": cookieToken,
 		}
-	} else {
-		// todo: add mainland china api
-		return nil, errors.ErrNotImplemented
+	} else { // cn api
+		accountApi = "https://bbs-api.mihoyo.com"
+		api = "https://api-takumi.mihoyo.com"
+		mark := "bbs_sign_reward"
+		actId = "e202009291139501"
+		accountInfoUrl = fmt.Sprintf("%s/game_record/card/api/getGameRecordCard", accountApi)
+		signUrl, signInfoUrl = utils.GetSignUrl(api, mark)
+		oversea = false
+		cookies = map[string]string{
+			"ltuid_v2":  accountId,
+			"ltoken_v2": cookieToken,
+		}
 	}
 	c.accountInfoRequest, err = request.NewRequest(
 		request.WithMethod(http.MethodGet),
